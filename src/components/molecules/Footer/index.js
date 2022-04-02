e6b39157f3585b0b7bc513258eadd73e13aa9cdc
raunak-mishra <mishraraunak326@gmail.com>
2022-04-02 12:33:28 +0530

    algo

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      <section>
+        
+      </section>
+
+      <section className="Footer__Items">
+        
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
